1c1e10763c27de522df1473f34fcb718a6bccd42
chrisdardas <85883633+chrisdardas@users.noreply.github.com>
2024-11-14 13:28:39 +0200

    Changes to Swagger File
    Necessary changes to the swagger file in order for our server to function properly.

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ var app = expressAppConfig.getApp();
 
 // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {
+    console.log("WHY NOT???");
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
 });
