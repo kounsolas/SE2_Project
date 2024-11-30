f8cbd60819a97bfd4e491d9ffccd470b1f0dc68b
chrisdardas <85883633+chrisdardas@users.noreply.github.com>
2024-11-30 22:36:50 +0200

    Update search.test.js

--- tests/search.test.js
+++ tests/search.test.js
@@ -35,12 +35,12 @@ test('Get /search returns correct response and status code', async(t) => {
         throw err;
     }
 });
-// Testing if the response time is lower than 200ms
+// Testing if the response time is lower than 500ms
 test('GET /search acceptable time response', async (t) => {
     const start = Date.now();
     await t.context.got('search');
     const duration = Date.now() - start;
-    t.true(duration < 200);
+    t.true(duration < 500);
 });
 // testing POST method
 test('POST /search returns 405 method not allowed', async(t) => {
