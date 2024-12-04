4d57d6b1a4a235440650df1f767ae575fb5e2241
chrisdardas <85883633+chrisdardas@users.noreply.github.com>
2024-12-04 15:45:38 +0200

    Update init.cy.js

--- cypress/e2e/init.cy.js
+++ cypress/e2e/init.cy.js
@@ -8,5 +8,5 @@ it('GET /search', () => {
     cy.visit("http://localhost:8080/docs/#/search/searchGET");
     cy.get("button.btn.try-out__btn").click();
     cy.get('button.btn.execute.opblock-control__btn').click();
-    cy.contains()
+    //cy.contains()
 })
