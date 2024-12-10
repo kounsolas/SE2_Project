3372c809312513654ed0efbc3cae3e15499bc99c
chrisdardas <85883633+chrisdardas@users.noreply.github.com>
2024-12-11 00:49:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(swaggerApp);
 if(require.main === module){ 
     http.createServer(app).listen(serverPort, function () {
         // console.log("WHY NOT???");
-        console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+        console.log('Your server2312 is listening on port %d (http://localhost:%d)', serverPort, serverPort);
         console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
     });
 }
