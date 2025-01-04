c397f4ec15e2ff589d859632da4565d720245d3f
tanikara <tanikara@ece.auth.gr>
2025-01-04 20:16:29 +0200

    commit changes for some violations
    in the directions.js file , in reservations.js file , in directionsService.js file and in reservationsService.js file

--- controllers/Directions.js
+++ controllers/Directions.js
@@ -3,7 +3,7 @@
 var utils = require('../utils/writer.js');
 var Directions = require('../service/DirectionsService');
 
-module.exports.directionsGET = function directionsGET(req, res, next, restaurantName) {
+module.exports.directionsGET = function directionsGET(_, res, _next, restaurantName) {
   Directions.directionsGET(restaurantName)
     .then(function (response) {
       if (!response) {
@@ -14,7 +14,7 @@ module.exports.directionsGET = function directionsGET(req, res, next, restaurant
         utils.writeJson(res, response);
       }
     })
-    .catch(function (error) {
+    .catch(function (_) {
       // Handle any internal errors
       utils.writeJson(res, { message: 'Internal server error' }, 500);
     });
