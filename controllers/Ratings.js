b840381339d2c97364d3d807d1836ce61d6f94e7
bekouvas <79990762+bekouvas@users.noreply.github.com>
2025-01-05 12:45:18 +0200

    Add more documantation on Ratings controller

--- controllers/Ratings.js
+++ controllers/Ratings.js
@@ -1,8 +1,16 @@
 'use strict';
-
+/**
+ * This module serves as the controller for handling `Ratings`-related API endpoints.
+ * It delegates the processing logic to the `RatingsService` and uses `utils/writer.js` for 
+ * formatting and sending responses to the client.
+ */ 
 var utils = require('../utils/writer.js');
 var Ratings = require('../service/RatingsService');
 
+/**
+ * GET /ratings
+ * Fetches all ratings for a specific restaurant.
+ */
 module.exports.ratingsGET = function ratingsGET(_, res, _next, restaurant_name) {
   Ratings.ratingsGET(restaurant_name)
     .then(function(response) {
@@ -13,6 +21,10 @@ module.exports.ratingsGET = function ratingsGET(_, res, _next, restaurant_name) 
     });
 };
 
+/**
+ * DELETE /ratings/:id
+ * Deletes a specific rating by its ID.
+ */
 module.exports.ratingsIdDELETE = function ratingsIdDELETE(_, res, _next, id) {
   Ratings.ratingsIdDELETE(id)
     .then(function(response) {
@@ -29,6 +41,10 @@ module.exports.ratingsIdDELETE = function ratingsIdDELETE(_, res, _next, id) {
     });
 };
 
+/**
+ * GET /ratings/:id
+ * Fetches the details of a specific rating by its ID and restaurant name.
+ */
 module.exports.ratingsIdGET = function ratingsIdGET(_, res, _next, restaurant_name, id) {
   //console.log('Controller params:', { id, restaurant_name });
   Ratings.ratingsIdGET(id, restaurant_name)
@@ -40,6 +56,10 @@ module.exports.ratingsIdGET = function ratingsIdGET(_, res, _next, restaurant_na
     });
 };
 
+/**
+ * PUT /ratings/:id
+ * Updates an existing rating by its ID and restaurant name.
+ */
 module.exports.ratingsIdPUT = function ratingsIdPUT(_, res, _next, body, restaurant_name, id) {
   Ratings.ratingsIdPUT(body, restaurant_name, id)
     .then(function(response) {
@@ -50,6 +70,10 @@ module.exports.ratingsIdPUT = function ratingsIdPUT(_, res, _next, body, restaur
     });
 };
 
+/**
+ * POST /ratings
+ * Creates a new rating for the restaurant "Mamalouka".
+ */
 module.exports.ratingsPOST = function ratingsPOST(_, res, _next, body) {
   const restaurant_name = 'Mamalouka'; // Hardcoded value
   // console.log('Received Body in Controller:', body);
